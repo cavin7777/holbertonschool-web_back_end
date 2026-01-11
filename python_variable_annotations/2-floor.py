3b7d9ff52d44f4baab138cf6882ce64d5ce1fa04
cavin7777 <caven7777@gmail.com>
2026-01-11 06:37:45 +0400

    2_0

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+This module provides a function that returns the floor of a floating-point number.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a floating-point number.
+    """
+    return math.floor(n)
