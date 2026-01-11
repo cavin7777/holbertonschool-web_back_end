c160977d0ad1388801d9688040820a4aef99ec15
cavin7777 <caven7777@gmail.com>
2026-01-11 06:46:21 +0400

    3_1_pycodestyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module provides a function that converts a float to its string representation.
+This module provides a function that converts a float to
+its string representation.
 """
 
 
