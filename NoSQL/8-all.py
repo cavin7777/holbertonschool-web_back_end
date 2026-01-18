a9395e9803f8f362ea6574d52eeb092f7942c6c9
cavin7777 <caven7777@gmail.com>
2026-01-18 22:58:03 +0400

    8_0

---
+++ NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module that lists all documents in a MongoDB collection
+"""
+
+def list_all(mongo_collection):
+    """
+    Returns all documents in a collection.
+    """
+    if mongo_collection is None:
+        return []
+
+    return list(mongo_collection.find())
